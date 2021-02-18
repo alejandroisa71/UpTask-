2fd721dd44c0b58b4ec18b8cadae1679a7c827e3
Alejandro Isa <alejandroisa@gmail.com>
2021-02-18 01:32:16 -0300

    Configuracion inicial de  las vistas

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const routes = require("./routes");
+const path = require("path");
 
 // crear una app de express
 const app = express();
+
+//habilitar Pug
+app.set("view engine", "pug");
+
+//Añadir las vistas
+app.set("views", path.join(__dirname, "./views"));
 
 app.use("/", routes());
 
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,5 @@ const proyectosController = require("../controllers/proyectosController");
 module.exports = function () {
   //ruta para el home
   router.get("/", proyectosController.proyectosHome);
-  router.get("/nosotros", proyectosController.nosotros);
   return router;
 };
