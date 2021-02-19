73c02a61a3e5dee8a7fa6432d9067b71ceb403f1
Alejandro Isa <alejandroisa71@gmail.com>
2021-02-19 19:58:53 -0300

    Se agrega formulario y se crea funcionalidad a traves de bodyParser para poder enviar y leer lo datos del mismo

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -9,3 +9,8 @@ exports.formularioProyecto =(req,res)=>{
     nombrePagina: 'Nuevo Proyecto'
   })
 }
+
+exports.nuevoProyecto = (req,res)=>{
+  //Enviar a la consola lo que el usuario escriba
+  console.log(req.body)
+}
