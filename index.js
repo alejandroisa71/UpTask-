20d66cc874e86e7c9330dac3cd9fc78895687cf9
Alejandro Isa <alejandroisa@gmail.com>
2021-02-22 01:53:36 -0300

    Se instala y configura sequelize y mysql2, se crea el archivo Proyectos.js en carpeta modelos y se crea la conexion con la BD

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const routes = require("./routes");
 const path = require("path");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+
+//crear la conexion a la base de datos
+const db = require("./config/db");
+
+// importar el modelo
+require("./models/Proyectos");
+
+db.sync()
+  .then(() => console.log("Conectado al Servidor"))
+  .catch((error) => console.log(error));
 
 // crear una app de express
 const app = express();
@@ -16,7 +26,7 @@ app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "./views"));
 
 //habilitar bodyParser para leer los datos del formulario
-app.use(bodyParser.urlencoded({extend:true}));
+app.use(bodyParser.urlencoded({ extend: true }));
 
 app.use("/", routes());
 
