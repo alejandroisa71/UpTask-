ab01234dcd0841cf7b4e7581bbc4cdda52795b8e
Alejandro Isa <alejandroisa@gmail.com>
2021-02-22 02:12:42 -0300

    se agregan registros con sequlize y Async

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -1,36 +1,40 @@
+const Proyectos = require("../models/Proyectos");
+
 exports.proyectosHome = (req, res) => {
   res.render("index", {
     nombrePagina: "Proyectos",
   });
 };
 
-exports.formularioProyecto =(req,res)=>{
-  res.render('nuevoProyecto',{
-    nombrePagina: 'Nuevo Proyecto'
-  })
-}
+exports.formularioProyecto = (req, res) => {
+  res.render("nuevoProyecto", {
+    nombrePagina: "Nuevo Proyecto",
+  });
+};
 
-exports.nuevoProyecto = (req,res)=>{
+exports.nuevoProyecto = async (req, res) => {
   //Enviar a la consola lo que el usuario escriba
   //console.log(req.body)
 
-//validar que tengamos algo en el input
-const {nombre} = req.body;
+  //validar que tengamos algo en el input
+  const { nombre } = req.body;
 
-let errores = [];
+  let errores = [];
 
-if(!nombre){
-  errores.push({'texto': 'Agrega un Nombre Al Proyecto'})
-}
+  if (!nombre) {
+    errores.push({ texto: "Agrega un Nombre Al Proyecto" });
+  }
 
-// si hay errores
-if (errores.length> 0) {
-  res.render('nuevoProyecto', {
-    nombrePagina: 'Nuevo Proyecto',
-    errores
-  })
-}else{
-  //no hay errores
-  //Insertar en la BD.
-}
-}
+  // si hay errores
+  if (errores.length > 0) {
+    res.render("nuevoProyecto", {
+      nombrePagina: "Nuevo Proyecto",
+      errores,
+    });
+  } else {
+    //no hay errores
+    //Insertar en la BD.
+    const proyecto = await Proyectos.create({ nombre });
+    res.redirect("/");
+  }
+};
