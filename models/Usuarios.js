f49901012d8e97a790ce76087b1e3683e986c0e9
Alejandro Isa <alejandroisa@gmail.com>
2021-02-28 03:18:58 -0300

    se hashea password con bcrypt

--- models/Usuarios.js
+++ models/Usuarios.js
@@ -1,23 +1,36 @@
-const { Sequelize } = require("sequelize");
-const Sequlize = require("sequelize");
+const Sequelize = require("sequelize");
 const db = require("../config/db");
 const Proyectos = require("../models/Proyectos");
+const bcrypt = require("bcrypt-nodejs");
 
-const Usuarios = db.define("usuarios", {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+const Usuarios = db.define(
+  "usuarios",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    email: {
+      type: Sequelize.STRING(60),
+      allowNull: false,
+    },
+    password: {
+      type: Sequelize.STRING(60),
+      allowNull: true,
+    },
   },
-  email: {
-    type: Sequelize.STRING(60),
-    allowNull: false,
-  },
-  password: {
-    type: Sequelize.STRING(60),
-    allowNull: true,
-  },
-});
+  {
+    hooks: {
+      beforeCreate(usuario) {
+        usuario.password = bcrypt.hashSync(
+          usuario.password,
+          bcrypt.genSaltSync(10)
+        );
+      },
+    },
+  }
+);
 
 Usuarios.hasMany(Proyectos);
 
