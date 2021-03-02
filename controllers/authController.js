ea51cd3a3ee40ba61b84d9c773bd475bacd649f0
Alejandro Isa <alejandroisa@gmail.com>
2021-03-02 03:24:27 -0300

    se verifica que la cuenta exista

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
 const passport = require("passport");
 
+const Usuarios = require("../models/Usuarios");
+
 //aqui se pone la estrategia por ejemplo: local, email, facebook
 exports.autenticarUsuario = passport.authenticate("local", {
   successRedirect: "/",
@@ -24,4 +26,20 @@ exports.cerrarSesion = (req, res) => {
   req.session.destroy(() => {
     res.redirect("/iniciar-sesion"); //al cerrar sesion no lleva al login
   });
+};
+
+//genera un token si el Usuario es valido
+exports.enviarToken = async (req, res, next) => {
+  //verificar que el usuario existe
+  const { email } = req.body;
+  const usuario = await Usuarios.findOne({ where: { email } });
+
+  //si no existe el usuario
+  if (!usuario) {
+    req.flash("error", "No existe esa Cuenta");
+    res.render("reestablecer", {
+      nombrePagina: "Reestablecer tu Contraseña",
+      mensajes: req.flash(),
+    });
+  }
 };
--- routes/index.js
+++ routes/index.js
@@ -92,6 +92,7 @@ module.exports = function () {
 
   //reestablecer contraseña
   router.get("/reestablecer", usuariosController.formRestablecerPassword);
+  router.post("/reestablecer", authController.enviarToken);
 
   return router;
 };
