ff2564607eafa49ed538c402012145751d44d210
Alejandro Isa <alejandroisa@gmail.com>
2021-03-02 02:00:41 -0300

    autorizacion de usuarios (si estan logueados), se protege rutas, cerrar sesion

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,27 @@
 const passport = require("passport");
 
-//aqu'i se pone la estrategia por ejemplo: local, email, facebook
+//aqui se pone la estrategia por ejemplo: local, email, facebook
 exports.autenticarUsuario = passport.authenticate("local", {
   successRedirect: "/",
   failureRedirect: "/iniciar-sesion",
   failureFlash: true,
   badRequestMessage: "Ambos Campos son Obligatorios",
 });
+
+//Funcion para revisar si el usuario esta logueado o no
+exports.usuarioAutenticado = (req, res, next) => {
+  // si el usuario esta autenticado, adelante
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  // si no esta untenticado, redirigir al formulario
+  return res.redirect("/iniciar-sesion");
+};
+
+//funcion para cerrar sesion
+exports.cerrarSesion = (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/iniciar-sesion"); //al cerrar sesion no lleva al login
+  });
+};
--- routes/index.js
+++ routes/index.js
@@ -12,37 +12,72 @@ const authController = require("../controllers/authController");
 
 module.exports = function () {
   //ruta para el home
-  router.get("/", proyectosController.proyectosHome);
-  router.get("/nuevo-proyecto", proyectosController.formularioProyecto);
+  router.get(
+    "/",
+    authController.usuarioAutenticado,
+    proyectosController.proyectosHome
+  );
+
+  router.get(
+    "/nuevo-proyecto",
+    authController.usuarioAutenticado,
+    proyectosController.formularioProyecto
+  );
   router.post(
     "/nuevo-proyecto",
+    authController.usuarioAutenticado,
     body("nombre").not().isEmpty().trim().escape(),
     proyectosController.nuevoProyecto
   );
 
   //listar Proyecto
-  router.get("/proyectos/:url", proyectosController.proyectoPorUrl);
+  router.get(
+    "/proyectos/:url",
+    authController.usuarioAutenticado,
+    proyectosController.proyectoPorUrl
+  );
 
   //Actualizar el Proyecto
-  router.get("/proyecto/editar/:id", proyectosController.formularioEditar);
+  router.get(
+    "/proyecto/editar/:id",
+    authController.usuarioAutenticado,
+    proyectosController.formularioEditar
+  );
   router.post(
     "/nuevo-proyecto/:id",
+    authController.usuarioAutenticado,
     body("nombre").not().isEmpty().trim().escape(),
     proyectosController.actualizarProyecto
   );
 
   //eliminar proyecto
 
-  router.delete("/proyectos/:url", proyectosController.eliminarProyecto);
+  router.delete(
+    "/proyectos/:url",
+    authController.usuarioAutenticado,
+    proyectosController.eliminarProyecto
+  );
 
   //Tareas
-  router.post("/proyectos/:url", tareasController.agregarTarea);
+  router.post(
+    "/proyectos/:url",
+    authController.usuarioAutenticado,
+    tareasController.agregarTarea
+  );
 
   //Actualizar Tarea
-  router.patch("/tareas/:id", tareasController.cambiarEstadoTarea);
+  router.patch(
+    "/tareas/:id",
+    authController.usuarioAutenticado,
+    tareasController.cambiarEstadoTarea
+  );
 
   //Eliminar Tarea
-  router.delete("/tareas/:id", tareasController.eliminarTarea);
+  router.delete(
+    "/tareas/:id",
+    authController.usuarioAutenticado,
+    tareasController.eliminarTarea
+  );
 
   //Crear nueva cuenta
   router.get("/crear-cuenta", usuariosController.formCrearCuenta);
@@ -51,6 +86,9 @@ module.exports = function () {
   //iniciar sesion
   router.get("/iniciar-sesion", usuariosController.formIniciarSesion);
   router.post("/iniciar-sesion", authController.autenticarUsuario);
+
+  //cerrar sesion
+  router.get("/cerrar-sesion", authController.cerrarSesion);
 
   return router;
 };
